bafe69d1721038bfcfb0c3bc0443b0d09d053297
daviavila2007 <139869720+daviavila2007@users.noreply.github.com>
2023-08-08 08:10:19 -0300

    Salvando

--- script.js
+++ script.js
@@ -5,32 +5,5 @@ function maioridade() {
     var idade3 = number(document.getElementById("idade3").value)
     var idade4 = number(document.getElementById("idade4").value)
     var idade5 = number(document.getElementById("idade5").value)
-    if(idade1 >= maioridade){
-        maioridade = idade1
-    }
-    else if(idade2 > maioridade){
-        maioridade = idade2
-    }
-    else if(idade3 > maioridade){
-        maioridade = idade3
-    }
-    else if(idade4 > maioridade){
-        maioridade = idade4
-    }
-    else if(idade5 > maioridade){
-        maioridade = idade5
-    }
-    else if(idade2 = maioridade){
-        alert("Mais de uma idade maior")
-    }
-    else if(idade3 = maioridade){
-        alert("Mais de uma idade maior")
-    }
-    else if(idade4 = maioridade){
-        alert("Mais de uma idade maior")
-    }
-    else if(idade5 = maioridade){
-        alert("Mais de uma idade maior")
-    }
-    alert("A maior idade é ", maioridade, "E éssa é a idade de ", nomedomaisvelho)
-    document.getElementById("rr").innerHTML = maioridade
+    Math.max(idade1, idade2, idade3, idade4, idade5);
+}
